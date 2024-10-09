dc506c92eeaadc5e8bbba9cf8730b2e46a78dcd1
Argen99 <argen1999@mail.ru>
2024-10-09 21:44:05 +0600

    удалил закомментированный код

--- core-ui/src/main/java/com/example/core_ui/utils/UIState.kt
+++ core-ui/src/main/java/com/example/core_ui/utils/UIState.kt
@@ -6,9 +6,3 @@ sealed class UIState<T> {
     class Error<T>(val error: String) : UIState<T>()
     class Success<T>(val data: T) : UIState<T>()
 }
-
-//class SearchScreen<T> (showAllVacancies: Boolean ,data: T) : UIState.Success<T>(data)
-//
-//sealed class CustomUIState<T> : UIState<T>() {
-//    class ShowAllVacancies<T>(val data: T) : CustomUIState<T>()
-//}
